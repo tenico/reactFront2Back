aa60f8ce0d21c20b8d1827bc183b8b3cd63e2e6d
David Silva <tenico94@gmail.com>
2023-03-23 14:35:43 +0000

    Hello World!

---
+++ feedback-app/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function App(){
+    return  React.createElement('div',{ className: 'container'},React.createElement('h1',{},'Hello World!'))
+    
+}
+
+export default App
